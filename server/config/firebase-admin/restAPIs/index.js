9d72eb71ef433710095b8500e57374de008c8133
mohsinghani <mohsinghani.777@gmail.com>
2019-08-14 03:59:42 +0500

    implement signin with firebase rest API

---
+++ server/config/firebase-admin/restAPIs/index.js
@@ -0,0 +1,4 @@
+
+module.exports = {
+    signIn: "https://identitytoolkit.googleapis.com/v1/accounts:signInWithPassword?key=AIzaSyAYmGtN6rN0nsDESiJC9Lvn94BbvCaclWE"
+}
--- server/resolvers/signin.js
+++ server/resolvers/signin.js
@@ -1,11 +1,23 @@
+const admin = require("firebase-admin");
+const fetch = require('node-fetch');
+const { signIn } = require("./../config/firebase-admin/restAPIs/index")
+
 const signinResolvers = {
     Mutation: {
-        signin: () => {
-            return {
-                username: "asdsad",
-                email: "mohsin@gmail.com",
-                password: 12
+        signin: async (parent, args, context, info) => {
+            const { email, password } = args
+            const result = await fetch(signIn, {
+                method: 'post',
+                body: JSON.stringify({ email, password }),
+                headers: { 'Content-Type': 'application/json' },
+            })
+            const user = await result.json()
+
+            if (user && user.idToken) {
+                const { localId } = user
+                return { uid: localId }
             }
+            else throw new Error(user);
         }
     }
 }
